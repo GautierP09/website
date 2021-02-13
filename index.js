9e8a04fa9dc12f4760ac615d40125f86455c92f6
Gautier D. PLANTE <>
2021-02-13 14:39:32 +0100

    new port

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get('/', function(req, res) {
 	res.send('<h1>Welcome to Node.js project setup</h1>')
 })
 
-app.listen(3000, function(){
+app.listen(process.env.PORT, function(){
 	console.log("Server started on port: 3000")
 })
